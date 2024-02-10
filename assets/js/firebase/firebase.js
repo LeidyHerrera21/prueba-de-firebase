067ef23c0fb14c22bd6f526458a240615390fb8d
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2024-02-10 12:31:38 -0600

    Add files via upload

--- assets/js/firebase/firebase.js
+++ assets/js/firebase/firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDicKEOAHicVyEnrxnub5lvnE91QRyOf88",
   authDomain: "prueba-firebase-b8f2a.firebaseapp.com",
@@ -13,9 +12,8 @@ const firebaseConfig = {
   storageBucket: "prueba-firebase-b8f2a.appspot.com",
   messagingSenderId: "1007963953724",
   appId: "1:1007963953724:web:f36bee17cc912e8c110139",
-  measurementId: "G-E3H44GT8LS"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
