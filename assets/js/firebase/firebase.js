8514247cc53c035e299c7694a7aac6293c3bf5f9
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2024-02-12 19:47:55 -0600

    Add files via upload

--- assets/js/firebase/firebase.js
+++ assets/js/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+import { } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
@@ -17,3 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = firebase.firebase();
